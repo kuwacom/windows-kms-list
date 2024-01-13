f563dde0caefed5cda1376907d65f92306093e27
kuwa <kuwa.com3@gmail.com>
2024-01-13 23:40:30 +0900

    init

---
+++ winser-2019-datacenter.bat
@@ -0,0 +1,3 @@
+slmgr /ipk WMDGN-G9PQG-XVVXX-R3X43-63DFG
+slmgr /skms kms.ddns.net:1688
+slmgr /ato
---
+++ winser-2022-datacenter.bat
@@ -0,0 +1,3 @@
+slmgr /ipk WX4NM-KYWYW-QJJR4-XV3QB-6VM33
+slmgr /skms kms.ddns.net:1688
+slmgr /ato
